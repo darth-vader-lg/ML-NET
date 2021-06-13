9b1206c3c9921781034dfe4694d15930e44a420c
darth-vader-lg <luigi.generale@gmail.com>
2021-06-13 14:37:13 +0200

    Version suffix and package PrefixId.
    - Added the definition of the version suffix for the projects and the prefix id in the packages of the build system in the eng/Versions.props file.
    - Can push packages on nuget.org and locally.
    - Solved some dotnet compilation warnings.
    - TensorFlow.NET package from LG.TensorFlow.NET (0.40.1)
    - TensorFlow redistributables package from LG.SciSharp.TensorFlow.Redist (for Windows build).
    
    Signed-off-by: darth-vader-lg <luigi.generale@gmail.com>

--- eng/common/publish-packages.ps1
+++ eng/common/publish-packages.ps1
@@ -1,56 +1,141 @@
 [CmdletBinding(PositionalBinding=$false)]
 Param(
-  [string][Alias('c')]$configuration = "Release",
-  [Parameter(ParameterSetName="api-key")][string][Alias('k')]$apiKey,
-  [string]$platform = $null,
-  [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
+    [string][Alias('c')]$configuration = "Release",
+    [string]$platform = $null,
+    [string][Alias('k', "api-key")]$apiKey,
+    [string][Alias('u')]$url,
+    [int][Alias('t')]$timeout = 3601, # Workaround for the timeout; it must not be a multiple of 60, otherwise it will be ignored
+    [switch][Alias('n')]$nuget,
+    [switch][Alias('g')]$github,
+    [switch][Alias('h')] $help,
+    [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
 )
 
-# Check for the api key consistence
-if (($apiKey -eq "") -and (-not (Test-Path env:GITHUB_NUPKG_PUSH_KEY) -or $env:GITHUB_NUPKG_PUSH_KEY -eq "")) {
-    Write-Error "Push apy-key not specified!"
-    Write-Error "Use the -api-key switch or define the environment variable GITHUB_NUPKG_PUSH_KEY"
-    exit 1
-}
-if ($apiKey -eq "") {
-    $apiKey = $env:GITHUB_NUPKG_PUSH_KEY
-}
-
-# List of projects to exclude
-$exclude = (`
-    "Microsoft.ML.DnnImageFeaturizer.AlexNet",`
-    "Microsoft.ML.DnnImageFeaturizer.ResNet18",`
-    "Microsoft.ML.DnnImageFeaturizer.ResNet50",`
-    "Microsoft.ML.DnnImageFeaturizer.ResNet101"`
-)
-
-# List of created packages in the selected configuration and platform
-if ($platform -ne "") {
-    $platformDir = $platform, $configuration -join "."
-}
-else {
-    $platformDir = $configuration
+function Print-Usage() {
+    Write-Host "  -configuration <value>  Build configuration: 'Debug' or 'Release' (short: -c)"
+    Write-Host "  -platform <value>       Platform configuration: 'x86', 'x64' or any valid Platform value"
+    Write-Host "  -url <value>            The url wher to publish (short: -u)"
+    Write-Host "  -nuget                  Publish on the standard NuGet.org index (short: -n)"
+    Write-Host "  -github                 Publish on the standard GitHub index (short: -g)"
+    Write-Host "  -api-key <value>        The api key with write rights for publishing (short: -k)"
+    Write-Host "  -timeout <value>        The timeout for publishing, in seconds (short: -t)"
+    Write-Host "  -help                   Print help and exit"
 }
 
-# List of projects and packages
-$projects = Get-ChildItem -Path "src" -Directory
-$packages = Get-ChildItem -Path ("artifacts", "packages", $platformDir, "Shipping" -join "\") -Filter "*.nupkg"
+try {
 
-# Push the packages
-Write-Output "Pushing the packages on GitHub..."
-foreach ($project in $projects) {
-    # Check if the project is excluded
-    if ($exclude.Contains($project.Name)) { continue }
-    foreach ($package in $packages) {
-        if ($package.Name.Contains($project.Name)) {
-            $version = $package.Name.Replace(($project.Name, "." -join ''), "")
-            $val = 0
-            if ([int]::TryParse($version[0], [ref]$val)) {
-                $version = $version.Replace(".nupkg", "")
-                # Push the package
-                dotnet nuget push $package.FullName --force-english-output --skip-duplicate --no-symbols true --timeout 600 --source "https://nuget.pkg.github.com/dart-vader-lg/index.json" --api-key $apiKey
-                break
+    if ($help -or (($null -ne $properties) -and ($properties.Contains('/help') -or $properties.Contains('/?'))) -or (-not $url -and -not $nuget -and -not $github)) {
+        Print-Usage
+        exit 0
+    }
+
+    # Set the scipt root if launched in PowerShell ISE
+    if (-not($PSScriptRoot) -and $psISE) { $scriptRoot = Split-Path $psISE.CurrentFile.FullPath } else { $scriptRoot = $PSScriptRoot }
+
+    # Check for the api key consistence
+    if (($apiKey -eq "") -and $nuget) {
+        if (-not (Test-Path env:NUGET_NUPKG_PUSH_KEY) -or $env:NUGET_NUPKG_PUSH_KEY -eq "") {
+            Write-Error "Push api-key not specified!"
+            Write-Error "Use the -api-key switch or define the environment variable NUGET_NUPKG_PUSH_KEY"
+            Print-Usage
+            exit 1
+        }
+    }
+    if (($apiKey -eq "") -and $github) {
+        if (-not (Test-Path env:GITHUB_NUPKG_PUSH_KEY) -or $env:GITHUB_NUPKG_PUSH_KEY -eq "") {
+            Write-Error "Push api-key not specified!"
+            Write-Error "Use the -api-key switch or define the environment variable GITHUB_NUPKG_PUSH_KEY"
+            Print-Usage
+            exit 1
+        }
+    }
+
+    # Directory relative to the selected platform
+    if ($platform -ne "") {
+        $platformDir = $platform, $configuration -join "."
+    }
+    else {
+        $platformDir = $configuration
+    }
+
+    # List of packages
+    $packages = Get-ChildItem -Path ([IO.Path]::Combine($scriptRoot, "..", "..", "artifacts", "packages", "$platformDir", "Shipping")) -Filter "*.nupkg"
+
+    # List of projects
+    $projects = Get-ChildItem -Path "src" -Directory
+
+    # List of projects to exclude
+    $exclude = (`
+        "Microsoft.ML.DnnImageFeaturizer.AlexNet",`
+        "Microsoft.ML.DnnImageFeaturizer.ResNet18",`
+        "Microsoft.ML.DnnImageFeaturizer.ResNet50",`
+        "Microsoft.ML.DnnImageFeaturizer.ResNet101"`
+    )
+
+    # Publish to url
+    if ($url) {
+        # Push the packages
+        Write-Output "Pushing the packages on $url..."
+        foreach ($project in $projects) {
+            # Check if the project is excluded
+            if ($exclude.Contains($project.Name)) { continue }
+            foreach ($package in $packages) {
+                if ($package.Name.Contains($project.Name)) {
+                    # Push the package
+                    $prms = @($package.FullName, "--force-english-output", "--no-symbols",  "true",  "--timeout", $timeout, "--source", $url)
+                    if (-not(Test-Path -PathType Container -Path $url)) { $prms = $prms + @("--skip-duplicate") }
+                    if ($key) { $prms = $prms + @("--api-key", $key) }
+                    dotnet nuget push $prms
+                    break
+                }
             }
         }
     }
+
+    # Publish on nuget
+    if ($nuget) {
+        # Push the packages
+        Write-Output "Pushing the packages on NuGet..."
+        if ($apiKey -eq "") { $key = $env:NUGET_NUPKG_PUSH_KEY } else { $key = $apiKey }
+        foreach ($project in $projects) {
+            # Check if the project is excluded
+            if ($exclude.Contains($project.Name)) { continue }
+            foreach ($package in $packages) {
+                if ($package.Name.Contains($project.Name)) {
+                    # Push the package
+                    dotnet nuget push $package.FullName --force-english-output --skip-duplicate --no-symbols true --timeout $timeout --source "https://api.nuget.org/v3/index.json" --api-key $key
+                    break
+                }
+            }
+        }
+    }
+
+    # Publish on GitHub
+    if ($github) {
+        # GitHub package destination
+        $pushOrigin = (((git remote -v) -match "(push)") -split '\t')[0]
+        $pushUrl = git remote get-url --push $pushOrigin
+        $user = Split-Path -leaf (Split-Path $pushUrl)
+        $source = "https://nuget.pkg.github.com/", $user, "/index.json" -join ''
+        # Push the packages
+        Write-Output "Pushing the packages on GitHub..."
+        if ($apiKey -eq "") { $key = $env:GITHUB_NUPKG_PUSH_KEY } else { $key = $apiKey }
+        foreach ($project in $projects) {
+            # Check if the project is excluded
+            if ($exclude.Contains($project.Name)) { continue }
+            foreach ($package in $packages) {
+                if ($package.Name.Contains($project.Name)) {
+                    # Push the package
+                    dotnet nuget push $package.FullName --force-english-output --skip-duplicate --no-symbols true --timeout $timeout --source $source --api-key $key
+                    break
+                }
+            }
+        }
+    }
+
+    exit 0
+}
+catch {
+    Write-Host $_.ScriptStackTrace
+    exit 1
 }
